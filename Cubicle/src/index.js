7b32894603ade358a129ccb77f7b881bea279f6b
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-17 11:47:32 +0200

    Setup static files and trim handlebars views.

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = 5050;
@@ -7,6 +8,9 @@ const PORT = 5050;
 app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
+
+const staticFiles = express.static(path.resolve(__dirname, 'public'));
+app.use(staticFiles);
 
 app.get('/', (req, res) => {
     res.render('index');
