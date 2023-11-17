1f92049a735e7d6f2fb2e17045f6e518aff060ee
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-17 15:36:19 +0200

    Add const file and express and handlebars configs.

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+const { PORT } = require('./constants');
 
 const app = express();
-const PORT = 5050;
 
-app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
-
-const staticFiles = express.static(path.resolve(__dirname, 'public'));
-app.use(staticFiles);
+expressConfig(app);
+handlebarsConfig(app);
 
 app.get('/', (req, res) => {
     res.render('index');
