f1614c72a9924c09b11fb4f0a4c3ac33ff828632
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-18 10:55:17 +0200

    Add home and cube controllers and routing.

--- Cubicle/src/index.js
+++ Cubicle/src/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+
 const { PORT } = require('./constants');
+const routes = require('./router');
 
 const app = express();
 
+// Configs
 expressConfig(app);
 handlebarsConfig(app);
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+// Routing
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
