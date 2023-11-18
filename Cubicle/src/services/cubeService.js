6700a022f42aa3ce7884ce843990defc34620f8b
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-18 15:35:28 +0200

    Add serch functionality.

--- Cubicle/src/services/cubeService.js
+++ Cubicle/src/services/cubeService.js
@@ -6,7 +6,13 @@ const cubes = [
         description: 'n/a',
         imageUrl: 'https://target.scene7.com/is/image/Target/GUEST_74983d16-c9f3-4e62-9051-2a6a9fff5660?wid=488&hei=488&fmt=pjpeg',
         difficultyLevel: '3'
-    }
+    },
+    {
+        name: 'Cube2',
+        description: 'bhgdukgs',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Scramble.svg/640px-Scramble.svg.png',
+        difficultyLevel: '3'
+      }
 ];
 
 exports.create = (cubeData) => {
@@ -21,6 +27,30 @@ exports.create = (cubeData) => {
     return newCube;
 }
 
-exports.getAll = () => {
-    return [...cubes];
+exports.getAll = (search, from, to) => {
+    let filterCubes = [...cubes];
+
+    if (search) {
+        filterCubes = filterCubes.filter((cube) =>
+            cube.name.toLowerCase().includes(search.toLowerCase())
+        );
+    }
+
+    if (from) {
+        filterCubes = filterCubes.filter(
+            (cube) => cube.difficultyLevel >= Number(from)
+        );
+    }
+
+    if (to) {
+        filterCubes = filterCubes.filter(
+            (cube) => cube.difficultyLevel <= Number(to)
+        );
+    }
+
+    return filterCubes;
 }
+
+exports.getSingleCube = (id) => {
+    return cubes.find((cube) => cube.id === id);
+};
