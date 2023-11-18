1e6eb40355d72c26d3d294235d260b2353d70a2a
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-18 15:39:38 +0200

    Add error handling and implement details view.

--- Cubicle/src/controllers/cubeController.js
+++ Cubicle/src/controllers/cubeController.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res) => {
-    console.log(cubeService.getAll());
     res.render('create');
 });
 
@@ -19,6 +18,18 @@ router.post('/create', (req, res) => {
     });
 
     res.redirect('/');
+});
+
+router.get("/:cubeId/details", (req, res) => {
+    const { cubeId } = req.params;
+    const cube = cubeService.getSingleCube(cubeId);
+
+    if (!cube) {
+        res.redirect("/404");
+        return;
+    }
+
+    res.render("details", { ...cube });
 });
 
 module.exports = router;
