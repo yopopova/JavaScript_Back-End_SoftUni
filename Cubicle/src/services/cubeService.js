8ceb1be7ea3dacffc0eca3cffa741b9b7504613a
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-20 15:37:08 +0200

    Implement getSingleCube, getAll with Cube model.

--- Cubicle/src/services/cubeService.js
+++ Cubicle/src/services/cubeService.js
@@ -21,8 +21,8 @@ exports.create = async (cubeData) => {
     return cube;
 }
 
-exports.getAll = (search, from, to) => {
-    let filterCubes = [...cubes];
+exports.getAll = async (search, from, to) => {
+    const filterCubes = await Cube.find().lean();
 
     if (search) {
         filterCubes = filterCubes.filter((cube) =>
@@ -45,6 +45,4 @@ exports.getAll = (search, from, to) => {
     return filterCubes;
 }
 
-exports.getSingleCube = (id) => {
-    return cubes.find((cube) => cube.id === id);
-};
+exports.getSingleCube = (id) => Cube.findById(id);
