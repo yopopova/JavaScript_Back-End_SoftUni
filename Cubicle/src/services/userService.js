f1385bfe7a99d42d354d780de2b54b60c0d90d1f
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-25 11:18:28 +0200

    Implement Login logic with bcrypt comparing.

--- Cubicle/src/services/userService.js
+++ Cubicle/src/services/userService.js
@@ -1,5 +1,22 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = (userData) => {
     return User.create(userData);
+}
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Invalid username or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid username or password!');
+    }
+
+    return user;
 }
