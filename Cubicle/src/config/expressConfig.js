4d9038ab55e2ea15fb38f671c1a8f4f1eea822ae
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-25 13:08:57 +0200

    Save cookie with jwt for logged in user on client.

--- Cubicle/src/config/expressConfig.js
+++ Cubicle/src/config/expressConfig.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const expressConfig = (app) => {
     const staticFiles = express.static(path.resolve(__dirname, '../public'));
     app.use(staticFiles);
 
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 }
 
 module.exports = expressConfig;
