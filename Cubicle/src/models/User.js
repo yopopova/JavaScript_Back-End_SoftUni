b435c49ac3e03eab48c162b232dbc7c0b1360607
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-27 16:55:17 +0200

    Add username and password validation.

--- Cubicle/src/models/User.js
+++ Cubicle/src/models/User.js
@@ -2,13 +2,36 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        minLength: [5, 'Username is too short!'],
+        match: [/^[A-Za-z0-9]+$/, 'Username should be with english letters and digits only!'],
+        unique: {
+            value: true,
+            message: 'Username already exists!'
+        }
+    },
+    password: {
+        type: String,
+        minLength: [8, 'Password is too short!'],
+        validate: {
+            validator: function(value) {
+                return /^[A-Za-z0-9]+$/.test(value);
+            }
+        },
+        message: 'Password should be with english letters and digits only!'
+    }
 });
+
+userSchema.path('username').validate(function (username) {
+    const user = mongoose.model('User').findOne({username});
+    return !!user;
+}, 'Username already exists!');
 
 userSchema.virtual('repeatPassword').set(function (value) {
     if (value !== this.password) {
-        throw new mongoose.MongooseError('password missmatch!');
+        throw new Error('password missmatch!');
     }
 });
 
