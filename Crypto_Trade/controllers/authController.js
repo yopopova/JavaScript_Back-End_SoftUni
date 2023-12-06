5ed350982966ef8f278b2c4cef4fd46384e27ce4
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-06 19:04:20 +0200

    Create user model and add routes.

---
+++ Crypto_Trade/controllers/authController.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const authService = require('../services/authService');
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, repeatPassword } = req.body;
+    await authService.regsiter(username, email, password, repeatPassword);
+});
+
+module.exports = router;
---
+++ Crypto_Trade/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    virtuals: {
+        repeatPassword: {
+            set(value) {
+                if (this.password !== value) {
+                    throw new mongoose.Error('Password missmath!');
+                }
+            }
+        }
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
