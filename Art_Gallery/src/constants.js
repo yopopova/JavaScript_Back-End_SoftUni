7648710f50e2aab6e86c425850342c7c6d91a10c
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-19 10:56:33 +0200

    Add routes, secret, and port.

---
+++ Art_Gallery/src/constants.js
@@ -0,0 +1,7 @@
+const CONSTANTS = {
+    PORT: 5050,
+    CONNECTION_STR: 'mongodb://127.0.0.1:27017/ArtGalleryDB',
+    SECRET: '9f173557-19c4-4278-8a04-526a19a1a3dd'
+}
+
+module.exports = CONSTANTS;
