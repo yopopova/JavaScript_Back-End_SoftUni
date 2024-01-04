a81539f08bc61dbb0476d002172c6372fd07806a
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-04 11:16:13 +0200

    Add secret, port, and DB name.

---
+++ Gaming_Team/src/constants.js
@@ -0,0 +1,7 @@
+const CONSTANTS = {
+    PORT: 3000,
+    CONNECTION_STR: 'mongodb://127.0.0.1:27017/GamingTeamDB',
+    SECRET: '874c7294-3094-4bf9-a2d8-3952a0b50986'
+}
+
+module.exports = CONSTANTS;
