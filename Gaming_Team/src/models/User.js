a0393cd5ef77357bf0f1c0904dcf5dc5f60ba532
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-04 11:28:55 +0200

    Create user model.

---
+++ Gaming_Team/src/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 10
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 4
+    }
+});
+
+userSchema.virtual('repeatPassword').set(function (value) {
+    if (value !== this.password) {
+        throw new Error('Password missmath!');
+    }
+});
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
