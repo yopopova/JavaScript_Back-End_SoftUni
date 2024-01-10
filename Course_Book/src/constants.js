39103aa21577d0f6cfc3d8d4e647ea847d5c3689
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-10 11:03:34 +0200

    Add app secret, port, and DB name.

---
+++ Course_Book/src/constants.js
@@ -0,0 +1,7 @@
+const CONSTANTS = {
+    PORT: 3000,
+    CONNECTION_STR: 'mongodb://127.0.0.1:27017/CourseBookDB',
+    SECRET: '1d9a1f90-c511-4720-a9d3-0badf9388e42'
+}
+
+module.exports = CONSTANTS;
