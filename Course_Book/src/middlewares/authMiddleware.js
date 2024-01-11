ff32bd3449d9fc20ab3e8cea9210d323a33b9739
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-11 11:33:58 +0200

    Set up app authentication.

---
+++ Course_Book/src/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('../lib/jwt');
+const { SECRET } = require('../constants');
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['token'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, SECRET);
+            req.user = decodedToken;
+            res.locals.user = decodedToken;
+            res.locals.isAuthenticated = true;
+            next();
+        } catch (error) {
+            console.log(error);
+            res.clearCookie('token');
+            res.redirect('/users/login');
+        }
+
+        return;
+    }
+
+    next();
+}
+
+exports.isAuth = async (req, res, next) => {
+    if (!req.user) {
+        res.redirect('/users/login');
+    }
+
+    next();
+}
