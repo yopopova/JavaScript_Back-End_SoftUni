1456931de298fb2f5f02fa15fba35ec5934cecb7
Yoanna Popova <yoanna.dp@gmail.com>
2025-08-12 08:15:13 +0300

    Add the skeleton from Crypto Trade task.

---
+++ Crypto_Trade_Skeleton/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+// const path = require('path');
+const routes = require('./routes');
+const { authentication } = require('./middlewares/authMiddleware');
+const { PORT, CONNECTION_STR } = require('./constants');
+
+const app = express();
+
+// // Express Config
+// app.use(express.static(path.resolve(__dirname, './public')));
+// app.use('/static', express.static('public')); // Take all paths, which starts with '/static' and remove it. Another way which doesn't work for me.
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false })); // <- Body-parser | Middleware which parse forms data from the POST requests and add it like an object into req.body.
+// ^ If we set up 'extended' to 'true', this means 'urlencoded' will use 'qs' library. This library is for more complicated options. We set up 'extended' to 'false', because we want to use easier options.
+
+// // Handlebars Config
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs'); // If we use root folder, we don't need to set up 'app.set('views', 'src/views')'.
+
+app.use(cookieParser());
+app.use(authentication); // After cookieParser, because we use cookies into authMiddleware.
+
+// Router
+app.use(routes);
+
+// Connect to the DB
+mongoose.set('strictQuery', false); // To remove deprication warning message from Mongoose in the terminal.
+mongoose.connect(CONNECTION_STR);
+
+app.listen(PORT, () => console.log(`The server is listening on port: ${PORT}...`));
---
+++ Crypto_Trade_Skeleton/services/authService.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+const jwt = require('../lib/jsonwebtoken');
+const { SECRET } = require('../constants');
+
+exports.findByUsername = (username) => User.findOne({ username });
+
+exports.findByEmail = (email) => User.findOne({ email });
+
+exports.regsiter = async (username, email, password, repeatPassword) => {
+    if (password !== repeatPassword) {
+        throw new Error('Password missmath!');
+    }
+
+    // const existingUser = await this.findByUsername(username);
+
+    const existingUser = await User.findOne({
+        $or: [
+            { email },
+            { username }
+        ]
+    });
+
+    if (existingUser) {
+        throw new Error('User exists!');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    User.create({ username, email, password: hashedPassword });
+
+    return this.login(email, password);
+}
+
+exports.login = async (email, password) => {
+    const user = await this.findByEmail(email);
+
+    if (!user) {
+        throw new Error('Invalid email or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid email or password!');
+    }
+
+    const payload = {
+        _id: user._id,
+        email,
+        username: user.username
+    }
+
+    const token = await jwt.sign(payload, SECRET);
+
+    return token;
+}
