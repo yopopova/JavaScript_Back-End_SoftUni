343cb4b8e9e9a201c20fa2157fd19837b33daf0f
Yoanna Popova <yoanna.dp@gmail.com>
2025-08-14 12:04:04 +0300

    Add new method to crypto service to get all records from the database.

--- Crypto_Trade/services/cryptoService.js
+++ Crypto_Trade/services/cryptoService.js
@@ -1,3 +1,5 @@
 const Crypto = require('../models/Crypto');
 
-exports.create = (cryptoData) => Crypto.create(cryptoData);
+exports.getAll = () => Crypto.find({}).lean();
+
+exports.create = (ownerId, cryptoData) => Crypto.create({ ...cryptoData, owner: ownerId });
