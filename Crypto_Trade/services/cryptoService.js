b432737f42b47d58be0f8f478dffcaddd64f5bb4
Yoanna Popova <yoanna.dp@gmail.com>
2025-08-14 14:47:14 +0300

    Add getOne and buy crypto methods.

--- Crypto_Trade/services/cryptoService.js
+++ Crypto_Trade/services/cryptoService.js
@@ -2,4 +2,16 @@ const Crypto = require('../models/Crypto');
 
 exports.getAll = () => Crypto.find({}).lean();
 
+exports.getOne = (cryptoId) => Crypto.findById(cryptoId).lean();
+
+exports.buy = async (userId, cryptoId) => {
+    const crypto = await Crypto.findById(cryptoId);
+    // TODO: check if the user has already bought the crypto
+    crypto.buyers.push(userId);
+    return crypto.save();
+
+    // Alternative to the upper code lines
+    // Crypto.findByIdAndUpdate(cryptoId, { $push: { buyers: userId } });
+}
+
 exports.create = (ownerId, cryptoData) => Crypto.create({ ...cryptoData, owner: ownerId });
